fe848ea52f016ca91ea01127b2bca0c190154aa4
Hello world <helloworld@MacBooks-MacBook-Air.local>
2023-08-01 23:21:35 +0800

    8sariin1

--- frontend/src/component/Signup.js
+++ frontend/src/component/Signup.js
@@ -5,7 +5,7 @@ import background1 from "../images/background1.jpg"
 export const Signup = () =>{
     return(
         <>
-        <img src={background1} className={styles.body}></img>
+        <img src="https://st3.depositphotos.com/1203257/12631/i/600/depositphotos_126312664-stock-photo-italian-dolomites-landscape.jpgs" className={styles.body}></img>
         </>
     )
 }
